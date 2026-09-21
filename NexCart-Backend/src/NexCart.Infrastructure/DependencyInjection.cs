9f71bdc1afa8de002830000e379ca0cc4fd8b411
Mohamed Taha Jdidi <mtjdidi@integrationobjects.com>
2026-09-21 10:21:37 +0100

    changes projects references

--- NexCart-Backend/src/NexCart.Infrastructure/DependencyInjection.cs
+++ NexCart-Backend/src/NexCart.Infrastructure/DependencyInjection.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NexCart.Infrastructure.Middleware;
-using NexCart.Persistence;
 
 namespace NexCart.Infrastructure;
 
